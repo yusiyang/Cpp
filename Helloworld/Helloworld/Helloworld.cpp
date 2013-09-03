2db3157fefa4c9f3c6b0be04bdc1f019c8730cfe
yusiyang <woshiysy@126.com>
2013-09-03 23:16:36 +0800

    Homework
    Helloworld

---
+++ Helloworld/Helloworld/Helloworld.cpp
@@ -0,0 +1,16 @@
+// Helloworld.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+#include <iostream>
+
+using namespace std;
+int main(){
+cout<<"Helloworld!"<<endl<<"I'm yusiyang";
+getchar();
+return 0;
+}
+
+
