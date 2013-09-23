9fafa570d5508eecce76958671b5a2aab7efdff3
yusiyang <woshiysy@126.com>
2013-09-23 14:07:21 +0800

    Fahrenhite and Centigrade(Second Edition)
    Fahrenhite and Centigrade(Second Edition)

--- Homework2/Homework2/Fahrenhite and Centigrade.cpp
+++ Homework2/Homework2/Fahrenhite and Centigrade.cpp
@@ -12,22 +12,41 @@ return f;
 }
 int main(){
 	int t;
+	while(true){
 cout<<"请按序号"<<endl<<"1:摄氏度转华氏度"<<endl<<"2:华氏度转摄氏度"<<endl;
 	cin>>t;
+	
+	if((t!=1)&&(t!=2)){
+	cout<<"请输入1或者2"<<endl;cin.clear();cin.sync();continue;
+}
+
+	
+	else{
 if(t==1){
 	double c,f;
 cout<<"请输入摄氏度:"<<endl;
-	cin>>c;
-cout<<"所对应的华氏度为:"<<ctof(c)<<endl;
-system("pause");
-main();
+if(cin>>c==0){
+	cout<<"请正确输入"<<endl;cin.clear();cin.sync();continue;
 }
-if(t==2){
+else {
+	cout<<"所对应的华氏度为:"<<ctof(c)<<endl;}
+cin.clear();cin.sync();
+system("pause");
+}
+
+else if(t==2){
 double c,f;
 cout<<"请输入华氏度:"<<endl;
-cin>>f;
-cout<<"所对应的摄氏度为:"<<ftoc(f)<<endl;
-system("pause");
-main();
+if(cin>>f==0){
+	cout<<"请正确输入"<<endl;cin.clear();cin.sync();continue;
 }
+else {
+	cout<<"所对应的摄氏度为:"<<ftoc(f)<<endl;}
+cin.clear();cin.sync();
+system("pause");
+}
+
+	}
+	}
+return 0;
 }
